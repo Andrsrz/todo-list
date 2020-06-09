6091c029dc9f0a4034544bcc6f1d49eb7fde3e17
Andres Ruiz <andrsruiz@gmail.com>
2020-06-09 12:15:55 -0500

    Add Project Class

---
+++ src/project.js
@@ -0,0 +1,27 @@
+const Project = () => {
+	/* Default values */
+	var title = "";
+	var description = "";
+	var todos = [];
+
+	/* Setters */
+	const setTitle = (newTitle) => title = newTitle;
+	const setDescription = (newDescription) => description = newDescription;
+	/* Getters */
+	const getTitle = () => title;
+	const getDescription = () => description;
+	const getTodos = () => todos;
+
+	const addTodo = (todo) => {
+		todos.push(todo);
+	}
+
+	const removeTodo = (index) => {
+		todos.splice(index, 1);
+	}
+
+	return { setTitle, setDescription, getTitle, getDescription, getTodos,
+			 addTodo, removeTodo };
+};
+
+export { Project };
