963928a54167353304a184944b4d42a295382ea7
Andres Ruiz <andrsruiz@gmail.com>
2020-06-09 13:24:11 -0500

    Add HTML Skeleton

--- src/dom.js
+++ src/dom.js
@@ -3,16 +3,22 @@ import './style/style.css';
 const Dom = (() => {
 	const body = document.body;
 	const main = document.createElement("main");
+	const projects = document.createElement("div");
+	const items = document.createElement("div");
 	const footer = document.createElement("footer");
 
 	const populatePage = () => {
+		main.appendChild(projects);
+		main.appendChild(items);
 		body.appendChild(main);
 		body.appendChild(footer);
 	}
 
 	const setStyle = () => {
 		body.id = "site-body";
-		main.id = "site-main;"
+		main.id = "site-main";
+		projects.id = "site-projects";
+		items.id = "site-items";
 		footer.id = "site-footer";
 	}
 
