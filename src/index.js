24e720a368368cee35b0b1ef92d004a9fc046f2a
Andres Ruiz <andrsruiz@gmail.com>
2020-06-09 10:52:32 -0500

    Add Scripts

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import myName from './myName.js';
+
+function component() {
+	const element = document.createElement('div');
+
+	// Using my function
+	element.innerHTML = myName('Andres');
+	return element;
+}
+
+document.body.appendChild(component());
